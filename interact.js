2ecb2a1a62584df42b11eaa99b8482e5831e6948
angela <angela@matic.network>
2020-04-08 18:35:06 +0530

    add browser code

---
+++ interact.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+// for interacting with the browser
+// window.addEventListener('load', () => {
+//   document.getElementById("get").onclick = () => {
+//     get().then((r) => {
+//       document.getElementById("resultOfGet").innerHTML = r
+//     })
+//   }
+//   document.getElementById("set").onclick = () => {
+//     val = document.getElementById("value").value
+//     set(val).then((r) => {
+//       document.getElementById("resultOfSet").innerHTML = r
+//     })
+//   }
+// })
